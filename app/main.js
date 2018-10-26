5ce80df971b312b6275dbef00c3690535cdf02a2
joisgori <00034715@uca.edu.sv>
2018-10-26 09:22:58 -0600

    último commiut

--- app/main.js
+++ app/main.js
@@ -15,9 +15,16 @@ MimeTypes = {
 }
 
 /*
-    ¿Cuál es la principal función del módulo HTTP?
+¿Cuál es la principal función del módulo HTTP?
+    dar soporte al protocolo característico de http para ejecutar las peticiones
 ¿Cuál es la principal función del módulo FileSystem?
-¿Qué es un MIME type?req 
+    The Node.js file system module te permite trabajar con el archivo 
+    del sistema en tu computadora
+
+¿Qué es un MIME type?
+    Los MIME Types (Multipurpose Internet Mail Extensions) son la manera standard de mandar contenido a través de la red. 
+    Los tipos MIME especifican tipos de datos, como por ejemplo texto, imagen, audio, etc. que los archivos contienen. 
+    Recuerde que debe utilizar el sufijo correcto para este tipo de archivo. 
 
 
 */
@@ -155,24 +162,38 @@ http.createServer((req, res) => {
 
 /*
 ¿Qué contienen las variables "req" y "res" en la creación del servidor?
+    req almacena la petición a enviar
+    res tiene guardada y devuelve la respuesta de la petición
+
 ¿La instrucción .listen(number) puede fallar? Justifique.
+    sí, el ese "number" (el puerto) ya está siendo escuchado por alguien más.
+
 ¿Por qué es útil la función "collectRequestData(...)"?
-*/
+    almacena los datos que se van guardando en la variable req
 
-
-
-/*
 ¿Para qué, además de conocer la dirección de la petición, es útil la variable "pathname"?
+    se usa de validación de las rutas en el servidor para cargar los documentos en el mismo
 
 ¿Qué contine el parametro "data"?
+    toda la información de los documentos a cargar en pantalla, lo que es leído por un FileSystem
 
 ¿Cuál es la diferencia entre brindar una respuesta HTML y brindar una CSS?
+    el primero ncesita de Mimetype textHTML y el archivo CSS se lee como un texto plano-.
 
 ¿Qué contiene la variable "result"?
+    Para el caso, son todos los datos que se enviaron en el formulario
+
 ¿Por qué con la variable "data" se debe aplicarse el metodo toString()? Justifique.
+    Facilitará el manejo de la información con la que se trabaja, al castearla a string.
 
 ¿Hay diferencia al quitar el control de peticiones para hojas CSS? Si sucedió algo distinto justifique por qué.
+    Sí, solo no se carga la página de estilos y se ve el html puro
+
 ¿Se puede inciar el servidor (node main.js) en cualquier sitio del proyecto? Cualquier respuesta justifique.
+    No, da error.
+
 Con sus palabras, ¿Por qué es importante aprender Node.js sin el uso de frameworks a pesar que estos facilitan el manejo de API's?
+    Para entender cómo funciona y qué hacen cada una de las variables que utilizamos, facilita el entendimiento de errores y una mejor
+    manera de entender los algoritmos de los mismos.
 */
 
