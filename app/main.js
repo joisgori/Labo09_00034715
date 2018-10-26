3751076719d0367311937f60e0b8f6dc129ea4db
joisgori <00034715@uca.edu.sv>
2018-10-26 08:52:45 -0600

    penúltimo commit

--- app/main.js
+++ app/main.js
@@ -17,34 +17,141 @@ MimeTypes = {
 /*
     ¿Cuál es la principal función del módulo HTTP?
 ¿Cuál es la principal función del módulo FileSystem?
-¿Qué es un MIME type?
+¿Qué es un MIME type?req 
 
 
 */
 
 http.createServer((req, res) => {
     //Control code.
-}).listen(8081);
 
-function collectRequestData(request, callback) {
-    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-    if (request.headers['content-type'] ===
-        FORM_URLENCODED) {
-        let body = '';
-        // Evento de acumulación de data.
-        request.on('data', chunk => {
-            body += chunk.toString();
-        });
-        //Data completamente recibida
-        request.on('end', () => {
-            callback(null, parse(body));
-        });
-    } else {
-        callback({
-            msg = `The content-type don't is equals to ${FORM_URLENCODED}`
+    function collectRequestData(request, callback) {
+        const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+        if (request.headers['content-type'] ===
+            FORM_URLENCODED) {
+            let body = '';
+            // Evento de acumulación de data.
+            request.on('data', chunk => {
+                body += chunk.toString();
+            });
+            //Data completamente recibida
+            request.on('end', () => {
+                callback(null, parse(body));
+            });
+        } else {
+            callback({
+                msg: `The content-type don't is equals to ${FORM_URLENCODED}`
+            });
+        }
+    }
+
+    /***** */
+
+    var pathname = url.parse(req.url).pathname;
+
+    if (pathname == "/") {
+        pathname = "../index.html";
+    }
+
+    if (pathname == "../index.html") {
+        //Petición de la página principal
+        fs.readFile(pathname, (err, data) => {
+
+            if (err) {
+                console.log(err);
+                // HTTP Status: 404 : NOT FOUND
+                // En caso de no haberse encontrado el archivo
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                }); return res.end("404 Not Found");
+            }
+            //Página encontrada
+            // HTTP Status: 200 : OK
+
+            res.writeHead(200, {
+                'Content-Type':
+                    MimeTypes[pathname.split('.').pop()] || 'text/html'
+            });
+
+            // Escribe el contenido de data en el body de la respuesta.
+            res.write(data.toString());
+
+            // Envía la respuesta
+            return res.end();
         });
     }
-}
+
+    if (req.method === 'POST' && pathname == '/cv') {
+        //Petición del formulario a traves del método POST
+        collectRequestData(req, (err, result) => {
+
+            if (err) {
+                res.writeHead(400, {
+                    'content-type': 'text/html'
+                });
+                return res.end('Bad Request');
+            }
+
+            fs.readFile("../templates/plantilla.html", function (err, data) {
+                if (err) {
+                    console.log(err);
+                    // HTTP Status: 404 : NOT FOUND
+                    // Content Type: text/plain
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html'
+                    });
+                    return res.end("404 Not Found");
+                }
+                res.writeHead(200, {
+                    'Content-Type': MimeTypes[pathname.split('.').pop()] || 'text/html'
+                });
+
+                //Variables de control. 
+
+                let parsedData = data.toString().replace('${dui}', result.dui)
+                    .replace("${lastname}", result.lastname)
+                    .replace("${firstname}", result.firstname)
+                    .replace("${gender}", result.gender)
+                    .replace("${civilStatus}", result.civilStatus)
+                    .replace("${birth}", result.birth)
+                    .replace("${exp}", result.exp)
+                    .replace("${tel}", result.tel)
+                    .replace("${std}", result.std);
+
+                res.write(parsedData);
+                return res.end();
+            });
+        });
+    }
+
+    if (pathname.split(".")[1] == "css") {
+        console.log(pathname);
+        //Petición de la hoja CSS
+        fs.readFile(".." + pathname, (err, data) => {
+
+            if (err) {
+                console.log(err);
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                }); return res.end("404 Not Found");
+            }
+
+            res.writeHead(200, {
+                'Content-Type': MimeTypes[pathname.split('.').pop()] || 'text/css'
+            });
+
+            // Escribe el contenido de data en el body de la respuesta.
+            res.write(data.toString());
+
+            // Envía la respuesta
+            return res.end();
+
+        });
+    }
+
+}).listen(8081);
+
+
 
 /*
 ¿Qué contienen las variables "req" y "res" en la creación del servidor?
@@ -52,106 +159,7 @@ function collectRequestData(request, callback) {
 ¿Por qué es útil la función "collectRequestData(...)"?
 */
 
-var pathname = url.parse(req.url).pathname;
 
-if (pathname == "/") {
-    pathname = "../index.html";
-}
-
-if (pathname == "../index.html") {
-    //Petición de la página principal
-    fs.readFile(pathname, (err, data) => {
-
-        if (err) {
-            console.log(err);
-            // HTTP Status: 404 : NOT FOUND
-            // En caso de no haberse encontrado el archivo
-            res.writeHead(404, {
-                'Content-Type': 'text/html'
-            }); return res.end("404 Not Found");
-        }
-        //Página encontrada
-        // HTTP Status: 200 : OK
-
-        res.writeHead(200, {
-            'Content-Type':
-                MimeTypes[pathname.split('.').pop()] || 'text/html'
-        });
-
-        // Escribe el contenido de data en el body de la respuesta.
-        res.write(data.toString());
-
-        // Envía la respuesta
-        return res.end();
-    });
-}
-
-if (req.method === 'POST' && pathname == '/cv') {
-    //Petición del formulario a traves del método POST
-    collectRequestData(req, (err, result) => {
-
-        if (err) {
-            res.writeHead(400, {
-                'content-type': 'text/html'
-            });
-            return res.end('Bad Request');
-        }
-
-        fs.readFile("../templates/plantilla.html", function (err, data) {
-            if (err) {
-                console.log(err);
-                // HTTP Status: 404 : NOT FOUND
-                // Content Type: text/plain
-                res.writeHead(404, {
-                    'Content-Type': 'text/html'
-                });
-                return res.end("404 Not Found");
-            }
-            res.writeHead(200, {
-                'Content-Type': mimeTypes[pathname.split('.').pop()] || 'text/html'
-            });
-
-            //Variables de control. 
-
-            let parsedData = data.toString().replace('${dui}', result.dui)
-                .replace("${lastname}", result.lastname)
-                .replace("${firstname}", result.firstname)
-                .replace("${gender}", result.gender)
-                .replace("${civilStatus}", result.civilStatus)
-                .replace("${birth}", result.birth)
-                .replace("${exp}", result.exp)
-                .replace("${tel}", result.tel)
-                .replace("${std}", result.std);
-
-            res.write(parsedData);
-            return res.end();
-        });
-    });
-}
-
-if (pathname.split(".")[1] == "css") {
-    //Petición de la hoja CSS
-    fs.readFile(".." + pathname, (err, data) => {
-
-        if (err) {
-            console.log(err);
-            res.writeHead(404, {
-                'Content-Type': 'text/html'
-            }); return res.end("404 Not Found");
-        }
-
-        res.writeHead(200, {
-            'Content-Type': mimeTypes[pathname.split('.').pop()] || 'text/css'
-        });
-
-        // Escribe el contenido de data en el body de la respuesta.
-        res.write(data.toString());
-
-        // Envía la respuesta
-        return res.end();
-
-    });
-}
 
 /*
 ¿Para qué, además de conocer la dirección de la petición, es útil la variable "pathname"?
@@ -162,5 +170,9 @@ if (pathname.split(".")[1] == "css") {
 
 ¿Qué contiene la variable "result"?
 ¿Por qué con la variable "data" se debe aplicarse el metodo toString()? Justifique.
+
+¿Hay diferencia al quitar el control de peticiones para hojas CSS? Si sucedió algo distinto justifique por qué.
+¿Se puede inciar el servidor (node main.js) en cualquier sitio del proyecto? Cualquier respuesta justifique.
+Con sus palabras, ¿Por qué es importante aprender Node.js sin el uso de frameworks a pesar que estos facilitan el manejo de API's?
 */
 
